0fdfd735ce8f2a241a56833a59c30e75e7e6db7c
KingBonfante <72134861+KingBonfante@users.noreply.github.com>
2024-05-01 01:10:13 +0000

    fix: Ajuste no Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header(){
     return(
-        <div className="p-4 border-b-2 md:flex justify-between lg:mt-8 mr-3 font-bold">
+        <div className="flex justify-between p-4 border-b-2">
             <h3>WDM</h3>
             <button className="bg-indigo-700 p-1 px-4 rounded">Purchase</button>
         </div>
