14e00666121aa20e038c46216dca47d3d66bd833
KingBonfante <72134861+KingBonfante@users.noreply.github.com>
2024-05-01 00:13:48 +0000

    fix: builts

--- app/components/Built.tsx
+++ app/components/Built.tsx
@@ -7,18 +7,88 @@ export default function Built(){
         <div>
             <h3 className="text-emerald-500 text-5xl font-semibold">We Help to built startup websites</h3>
         </div>
-        <div>
-            <div  className="mt-5 rounded-2xl bg-[#4d4d4d]">
-                <div className="rounded-1xl bg-gray-800 ">
+        <div className="text-left">
+            <div  className="p-2 mx-2 mt-5 rounded-2xl bg-[#4d4d4d]">
+                <div className="m-2">
                     <img src="/img/img1.png" alt="" />
                 </div>
                 <div>
-                    <h4>Website Builders</h4>
+                    <h4 className=" my-2">Website Builders</h4>
                     <p className="font-light">Creating stunning, user-friendly websites tailored to your brand, with expertise in design, development, and online success.</p>
                 </div>
                 <div>
                     <a href="https://google.com">
-                        <p>Learn more ➞</p>
+                        <p className="mt-2">Learn more ➞</p>
+                    </a>
+                 </div>
+            </div>
+            <div  className="p-2 mx-2 mt-5 rounded-2xl bg-[#4d4d4d]">
+                <div className="m-2">
+                    <img src="/img/img2.png" alt="" />
+                </div>
+                <div>
+                    <h4 className="my-2">Content Management System</h4>
+                    <p className="font-light text-left">Streamline website management with a user-friendly platform for easy content creation, editing, and organization.</p>
+                </div>
+                <div>
+                    <a href="https://google.com">
+                        <p className="mt-2">Learn more ➞</p>
+                    </a>
+                 </div>
+            </div>
+            <div  className="p-2 mx-2 mt-5 rounded-2xl bg-[#4d4d4d]">
+                <div className="m-2">
+                    <img src="/img/img3.png" alt="" />
+                </div>
+                <div>
+                    <h4 className="my-2">Web Design</h4>
+                    <p className="font-light">User-centric web design. Elevate your online presence with beautiful, functional websites tailored to your unique needs.</p>
+                </div>
+                <div>
+                    <a href="https://google.com">
+                        <p className="mt-2">Learn more ➞</p>
+                    </a>
+                 </div>
+            </div>
+            <div  className="p-2 mx-2 mt-5 rounded-2xl bg-[#4d4d4d]">
+                <div className="m-2">
+                    <img src="/img/img4.png" alt="" />
+                </div>
+                <div>
+                    <h4 className="my-2">Hosting Service</h4>
+                    <p className="font-light">Reliable and secure hosting solutions for website performance, ensuring optimal speed and uptime for online presence.</p>
+                </div>
+                <div>
+                    <a href="https://google.com">
+                        <p className="mt-2">Learn more ➞</p>
+                    </a>
+                 </div>
+            </div>
+            <div  className="p-2 mx-2 mt-5 rounded-2xl bg-[#4d4d4d]">
+                <div className="m-2">
+                    <img src="/img/img5.png" alt="" />
+                </div>
+                <div>
+                    <h4 className="my-2">E-Commerce Solutions</h4>
+                    <p className="font-light">Scalable e-commerce solutions that drive online sales, enhance user experience, and boost business growth.</p>
+                </div>
+                <div>
+                    <a href="https://google.com">
+                        <p className="mt-2">Learn more ➞</p>
+                    </a>
+                 </div>
+            </div>
+            <div  className="p-2 mx-2 mt-5 rounded-2xl bg-[#4d4d4d]">
+                <div className="m-2">
+                    <img src="/img/img6.png" alt="" />
+                </div>
+                <div>
+                    <h4 className="my-2">Web Development</h4>
+                    <p className="font-light">Expertly crafting functional and dynamic websites, tailored to your unique needs and designed to captivate your audience.</p>
+                </div>
+                <div>
+                    <a href="https://google.com">
+                        <p className="mt-2">Learn more ➞</p>
                     </a>
                  </div>
             </div>
